562c3fab21309f4d0caece376c1cdcce3dc1b6a4
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-08-27 15:35:18 +0200

    updates install generator

---
+++ lib/generators/spree_line_item_discount/install/install_generator.rb
@@ -0,0 +1,25 @@
+module SpreeLineItemDiscount
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      class_option :auto_run_migrations, :type => :boolean, :default => false
+
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_line_item_discount\n"
+      end
+
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_line_item_discount'
+      end
+
+      def run_migrations
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
+      end
+    end
+  end
+end
