5e6778717ab19f667f0463e606e47a7f5537281c
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-08-27 15:33:59 +0200

    fixes engine and factories filename

--- lib/spree_line_item_discount/engine.rb
+++ lib/spree_line_item_discount/engine.rb
@@ -1,8 +1,8 @@
-module SpreeLine-item-discount
+module SpreeLineItemDiscount
   class Engine < Rails::Engine
     require 'spree/core'
     isolate_namespace Spree
-    engine_name 'spree_line-item-discount'
+    engine_name 'spree_line_item_discount'
 
     # use rspec for tests
     config.generators do |g|
@@ -13,6 +13,10 @@ module SpreeLine-item-discount
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+    end
+
+    initializer "spree.register.promotion_action" do |app|
+      app.config.spree.promotions.actions << Spree::Promotion::Actions::CreateVariantAdjustments
     end
 
     config.to_prepare &method(:activate).to_proc
