17af0673bbd9d0d595c4b66fa97f84bcbc61b353
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-08-29 17:51:20 +0200

    moves variant picker in it's own file

---
+++ app/assets/javascripts/spree/backend/variant_picker.js
@@ -0,0 +1,44 @@
+$.fn.variantPicker = function () {
+  'use strict';
+
+  this.select2({
+    minimumInputLength: 1,
+    multiple: true,
+    initSelection: function (element, callback) {
+      $.get(Spree.routes.variants_api, {
+        q: { id_in: element.val().split(',') },
+        token: Spree.api_key
+      }, function (data) {
+        callback(data.variants);
+      });
+    },
+    ajax: {
+      url: Spree.routes.variants_api,
+      datatype: 'json',
+      data: function (term, page) {
+        return {
+          q: {
+            product_name_or_sku_cont: term,
+          },
+          token: Spree.api_key
+        };
+      },
+      results: function (data, page) {
+        var variants = data.variants ? data.variants : [];
+        return {
+          results: variants
+        };
+      }
+    },
+    formatResult: function (variant) {
+      return variant.name + ' ' + variant.sku;
+    },
+    formatSelection: function (variant) {
+      return variant.name + ' ' + variant.sku;
+    }
+  });
+};
+
+$(document).ready(function () {
+  $('.variant_picker').variantPicker();
+});
